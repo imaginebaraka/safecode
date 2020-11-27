ef6fab6607fba725f118315792cdd1c3829341e5
Baraka Mulumia <barakamulumia@gmail.com>
2020-11-27 23:52:54 +0300

    generating a random code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import "./App.css";
+import safecode from './safecode';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [code, setCode] = useState(safecode(8));
+  const [len, setLen] = useState(0);
+  const [msg, setMsg] = useState("Suggested safe code is");
+
+  const handleChange = () => {
+    if (len) {
+      if (len < 8) {
+        alert("length too small");
+        setCode(safecode(8));
+        setMsg("Suggested safe code is")
+      } else {
+        setCode(safecode(len));
+        setMsg('Your safe code is')
+      }
+    } else {
+      setCode(safecode(8));
+      setMsg("Suggested safe code is")
+    }
+  };
+
   return (
     <div className="App">
       <header className="header">
@@ -15,6 +38,7 @@ function App() {
             id="length"
             className="length-input"
             placeholder="Enter desired safecode length"
+            onChange={(e) => setLen(Number(e.target.value))}
           />
         </div>
         <p className="lead">Pick a Color</p>
@@ -25,7 +49,7 @@ function App() {
               { id: 2, color: "green" },
               { id: 3, color: "blue" },
             ].map(({ id, color }) => (
-              <button className={`btn btn-${color} btn-color`} type="button">
+              <button key={id} className={`btn btn-${color} btn-color`} type="button">
                 {id}
               </button>
             ))}
@@ -33,13 +57,13 @@ function App() {
         </div>
       </form>
       <div className="output">
-        <p className="lead">suggested code:</p>
-        <p className="lead code">####</p>
+        <p className="lead">{msg}</p>
+        <p className="lead code">{code}</p>
       </div>
       <div className="controls form-group">
         <button className="btn btn-control">Prev Code</button>
         <button className="btn btn-control">Copy</button>
-        <button className="btn btn-control">New Code</button>
+        <button className="btn btn-control" onClick={handleChange}>New Code</button>
       </div>
     </div>
   );
---
+++ src/safecode.js
@@ -0,0 +1,35 @@
+import _ from "lodash";
+
+let characters = [
+  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  ["@", "_", "-", "!", "#", "$", "%", "&"],
+  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+];
+
+let sporadic = arr => arr[_.random(0, arr.length - 1)];
+
+function produce() {
+  let code = "";
+  for (let i = 0; i < characters.length; i++) {
+    code += sporadic(characters[i])
+  }
+  return code;
+}
+
+export default function safecode(len) {
+
+  let code = "";
+  let numOfLoops = Math.floor(len / 4);
+  let residue = _.round(len % 4, 1);
+
+  for (let i = 0; i < numOfLoops; i++) {
+    code += produce();
+  }
+
+  if (residue > 0) {
+    code += produce().slice(0, residue);
+  }
+
+  return code;
+}
