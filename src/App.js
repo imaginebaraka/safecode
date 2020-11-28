14b4adfcec9358708ae98ed0cc5e3741f29eca91
Baraka Mulumia <barakamulumia@gmail.com>
2020-11-28 21:47:55 +0300

    Implimented copying the code to clipboard

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
 import "./App.css";
-import safecode from './safecode';
-import React, { useState } from 'react';
+import safecode from "./safecode";
+import React, { useState, useEffect } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function App() {
-
   const [code, setCode] = useState(safecode(8));
   const [len, setLen] = useState(0);
   const [msg, setMsg] = useState("Suggested safe code is");
   const [systematic, setSystematic] = useState(false);
+  const [copySuccess, setCopySuccess] = useState(false);
 
   const handleChange = () => {
     if (len) {
       if (len < 8) {
         alert("length too small");
         setCode(safecode(8, systematic));
-        setSystematic(v => !v)
-        setMsg("Suggested safe code is")
+        setSystematic((v) => !v);
+        setMsg("Suggested safe code is");
       } else {
         setCode(safecode(len, systematic));
-        setMsg('Your safe code is');;
-        setSystematic(v => !v)
+        setMsg("Your safe code is");
+        setSystematic((v) => !v);
       }
     } else {
       setCode(safecode(8, systematic));
       setMsg("Suggested safe code is");
-      setSystematic(v => !v)
+      setSystematic((v) => !v);
     }
   };
+
+  const handleCopy = () => {
+    setCopySuccess(true);
+  };
+
+  useEffect(() => {
+    if (copySuccess) {
+      setTimeout(() => {
+        setCopySuccess(false);
+      }, 1000);
+    }
+  }, [copySuccess]);
 
   return (
     <div className="App">
@@ -52,24 +65,39 @@ function App() {
             { id: 1, color: "red" },
             { id: 2, color: "green" },
             { id: 3, color: "blue" },
-            { id: 4, color: "black" }
+            { id: 4, color: "black" },
           ].map(({ id, color }) => (
-            <button key={id} className={`btn btn-${color} btn-color`} type="button">
+            <button
+              key={id}
+              className={`btn btn-${color} btn-color`}
+              type="button"
+            >
               {id}
             </button>
           ))}
-
         </div>
       </div>
       <div className="output">
         <p className="lead">{msg}</p>
         <p className="lead code">{code}</p>
       </div>
+
       <div className="controls form-group">
-        <button className="btn btn-control">Prev Code</button>
-        <button className="btn btn-control">Copy</button>
-        <button className="btn btn-control" onClick={handleChange}>New Code</button>
+        <CopyToClipboard
+          onCopy={handleCopy}
+          options={{ message: "Whoa!" }}
+          text={code}
+        >
+          <button className="btn btn-control" onClick={handleCopy}>
+            Copy
+          </button>
+        </CopyToClipboard>
+        <button className="btn btn-control" onClick={handleChange}>
+          New Code
+        </button>
       </div>
+
+      {copySuccess && <p className="lead code">code copied successfully</p>}
     </div>
   );
 }
